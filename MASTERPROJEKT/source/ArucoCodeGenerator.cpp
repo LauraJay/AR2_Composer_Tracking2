baf3f12844f993f7f873398aa782863af78454c7
VeraBrockmeyer <vera.brockmeyer@googlemail.com>
2016-11-04 23:31:00 +0100

    push for debugging opencv_contrib lib

--- MASTERPROJEKT/source/ArucoCodeGenerator.cpp
+++ MASTERPROJEKT/source/ArucoCodeGenerator.cpp
@@ -1,37 +1,33 @@
-//#pragma once
-//#include <opencv2/opencv.hpp>
-//#include <opencv2/highgui.hpp>
-//#include <opencv2/aruco.hpp>
-//
-//using namespace cv;
-//
-//int main(int argc, char *argv[]) {
-//
-//	int dictionaryId = aruco::DICT_4X4_50; // Anzahl der Bits und Max Anz. der IDS
-//	int borderBits = 1; // Breite des Rands
-//	int markerSize = 500; // Ausgabebildgröße
-//
-//	String out = "ARUCO_ID";
-//	int nMarkerToPrint = 4;
-//	
-//
-//	aruco::Dictionary* dictionary =	aruco::getPredefinedDictionary(aruco::PREDEFINED_DICTIONARY_NAME(dictionaryId));
-//
-//	// evtl. von 1 an
-//	for (int i = 0; i < nMarkerToPrint; i++)
-//	{
-//
-//	Mat markerImg;
-//	aruco::drawMarker(dictionary, i, markerSize, markerImg, borderBits);
-//
-//	if (!markerImg.empty()) {
-//		imshow("marker", markerImg);
-//		waitKey(0);
-//		out+=(""+i);
-//		out += ".png";
-//	imwrite(out, markerImg);
-//	out = "ARUCO_ID";
-//	}
-//	}
-//		return 0;
-//}
+#pragma once
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/aruco.hpp>
+using namespace cv;
+// ATTENTION!!!! Comment main() in Main before compiling
+int main() {
+	int borderBits = 1; // Breite des Rands
+	int markerSize = 500; // Ausgabebildgröße
+	String out = "ARUCO_ID";
+	int nMarkerToPrint = 4;
+	
+	aruco::Dictionary dictionary = aruco::getPredefinedDictionary(aruco::PREDEFINED_DICTIONARY_NAME::DICT_4X4_50);
+	//aruco::Dictionary* dictionary =	aruco::getPredefinedDictionary(aruco::DICT_4X4_100);
+
+	//// evtl. von 1 an
+	//for (int i = 0; i < nMarkerToPrint; i++)
+	//{
+
+	//Mat markerImg;
+	//aruco::drawMarker(dictionary, i, markerSize, markerImg, borderBits);
+
+	//if (!markerImg.empty()) {
+	//	imshow("marker", markerImg);
+	//	waitKey(0);
+	//	out+=(""+i);
+	//	out += ".png";
+	//imwrite(out, markerImg);
+	//out = "ARUCO_ID";
+	//}
+	//}
+		return 0;
+}
--- MASTERPROJEKT/source/Main.cpp
+++ MASTERPROJEKT/source/Main.cpp
@@ -23,157 +23,157 @@ Main::Main() {
 }
 
 
-int main()
-{
-
-	std::cout << CV_VERSION << std::endl;
-
-	MarkerManagement* mm = new MarkerManagement();
-	std::array<Marker*,200> marker;
-	std::vector<int> takenIdVec;
-
-#ifdef TCP
-	//start TCP
-	TCP_output* out = new TCP_output();
-	out->startTCPServer();
-#endif 	// TCP
-
-
-#ifdef VIDEOLAURA
-	VideoCapture cap("C:/Users/AR/Desktop/Laura/02_Testmaterial/Drehung1.avi");
-	//VideoCapture cap("C:/Users/AR/Desktop/Laura/02_Testmaterial/Drehung2.avi");
-	//VideoCapture cap("C:/Users/AR/Desktop/Laura/02_Testmaterial/Lift1.avi");
-	//VideoCapture cap("C:/Users/AR/Desktop/Laura/02_Testmaterial/Lift2.avi");
-	//VideoCapture cap("C:/Users/AR/Desktop/Laura/02_Testmaterial/Register1.avi");
-	//VideoCapture cap("C:/Users/AR/Desktop/Laura/02_Testmaterial/reinraus1.avi");
-	//VideoCapture cap("C:/Users/AR/Desktop/Laura/02_Testmaterial/reinraus2.avi");
-	//VideoCapture cap("C:/Users/AR/Desktop/Laura/02_Testmaterial/Swap1.avi");
-	//VideoCapture cap("C:/Users/AR/Desktop/Laura/02_Testmaterial/Swap2.avi");
-	//VideoCapture cap("C:/Users/AR/Desktop/Laura/02_Testmaterial/totalCover1.avi");
-	//VideoCapture cap("C:/Users/AR/Desktop/Laura/02_Testmaterial/totalCover2.avi");
-
-	//alt
-	//VideoCapture cap("C:/Users/AR/Desktop/Laura/01_Testmaterial/001_A_Ohne_Verdeckung.avi");
-	//VideoCapture cap("C:/Users/AR/Desktop/Laura/01_Testmaterial/001_B_Ohne_Verdeckung.avi");
-	//VideoCapture cap("C:/Users/AR/Desktop/Laura/01_Testmaterial/006_Nacheinander_Hineinschieben.avi");
-	//VideoCapture cap("C:/Users/AR/Desktop/Laura/01_Testmaterial/002_A_Nichtmarkierte_Ecken_verdeckt.avi");
-
-	if (!cap.isOpened())  // check if we succeeded
-		return -1;
-#endif // VIDEOLAURA
-
-#ifdef VIDEOVERA
-	//Einbindung Video Vera 
-	VideoCapture cap("F:/Master/Masterprojekt/Testvideos/001_A_Ohne_Verdeckung.avi");
-	//VideoCapture cap("F:/Master/Masterprojekt/Testvideos/001_B_Ohne_Verdeckung.avi");
-	//VideoCapture cap("F:/Master/Masterprojekt/Testvideos/002_A_Nichtmarkierte_Ecken_verdeckt.avi");
-	//VideoCapture cap("F:/Master/Masterprojekt/Testvideos/002_B_Nichtmarkierte_Ecken_verdeckt.avi");
-	//VideoCapture cap("F:/Master/Masterprojekt/Testvideos/003_A_Markierte_Ecke_verdeckt.avi");
-	//VideoCapture cap("F:/Master/Masterprojekt/Testvideos/003_B_Markierte_Ecke_verdeckt.avi");
-	//VideoCapture cap("F:/Master/Masterprojekt/Testvideos/004_A_Person_verdeckt_Marker.avi");
-	//VideoCapture cap("F:/Master/Masterprojekt/Testvideos/004_B_Person_verdeckt_Marker.avi");
-	//VideoCapture cap("F:/Master/Masterprojekt/Testvideos/005_A_Farbige_Aermel.avi");
-	//VideoCapture cap("F:/Master/Masterprojekt/Testvideos/005_B_Farbige_Aermel.avi");
-	//VideoCapture cap("F:/Master/Masterprojekt/Testvideos/006_Nacheinander_Hineinschieben.avi");
-	//VideoCapture cap("F:/Master/Masterprojekt/Testvideos/reinraus1.avi");
-
-	if (!cap.isOpened())  // check if we succeeded
-		return -1;
-#endif // VIDEOVERA
-
-
-
-#ifdef uEYE
-	// uEye Caputure
-	uEye_input* uei = new uEye_input();
-	uei->inituEyeCam();
-#endif // uEYE
-
-
-	int counter = -1;
-	Mat frame;
-#ifdef uEYE
-	for (int i = 0; i < 30; i++)
-	{
-		frame = uei->getCapturedFrame();
-	}
-#endif //uEYE
-
-	namedWindow("edges", 1);
-	//LUT aufstellen
-
-	while (true)
-	{
-		counter++;
-		printf("COUNTER %i \n ", counter);
-#ifdef uEYE
-		frame = uei->getCapturedFrame();
-#endif // uEYE
-
-		//// Mark Middel for Calibration
-		//line(frame, Point2f(630, 512), Point2f(650, 512), Scalar(255, 0, 0), 2, CV_AA);
-		//line(frame, Point2f(640, 502), Point2f(640, 522), Scalar(255, 0, 0), 2, CV_AA);
-		//cv::imshow("Captured Video", frame);
-		//if (cv::waitKey(1) >= 0) break;
-
-#ifdef VIDEOLAURA
-		cap >> frame; // get a new frame from camera
-#endif // VIDEOLAURA
-
-
-#ifdef VIDEOVERA
-		cap >> frame; // get a new frame from camera
-#endif // VIDEOVERA
-
-		if (!frame.empty()) {
-			cvtColor(frame, frame, COLOR_BGR2HSV);
-			// run Marker Detection
-			MarkerDetection* md = new MarkerDetection();
-			int sucess = md->runMarkerDetection(frame);
-			if (sucess == 1) {
-				std::vector<RotatedRect> rects = md->getDetectedRects();
-				std::vector<unsigned char> markedCorners = md->getMarkedCorners();
-
-				// LUT Kalibration des Rechtecks
-				//run MarkerManagement
-
-				for (int i = 0; i < rects.size(); i++)
-				{
-						mm->trackMarker(rects[i], markedCorners[i], frame.size());
-				}
-				marker = mm->getTrackedMarker();
-				takenIdVec = mm->getTakenIDVec();
-
-				delete md;
-
-			}
-			else { marker = mm->getTrackedMarker(); }
-			debug(frame, marker, counter,takenIdVec);
-
-			imshow("edges", frame);
-			if (waitKey(4) >= 0)break;
-		}
-		else break;
-
-#ifdef TCP
-		//Send Markerdata via TCP
-		out->sendTCPData(marker,takenIdVec);
-
-#endif // TCP
-	}
-#ifdef uEYE
-	uei->exitCamera();
-	delete uei;
-#endif // uEYE
-
-	delete mm;
-
-
-#ifdef TCP
-	delete out;
-#endif // TCP
-	return EXIT_SUCCESS;
-}
+//int main()
+//{
+//
+//	std::cout << CV_VERSION << std::endl;
+//
+//	MarkerManagement* mm = new MarkerManagement();
+//	std::array<Marker*,200> marker;
+//	std::vector<int> takenIdVec;
+//
+//#ifdef TCP
+//	//start TCP
+//	TCP_output* out = new TCP_output();
+//	out->startTCPServer();
+//#endif 	// TCP
+//
+//
+//#ifdef VIDEOLAURA
+//	VideoCapture cap("C:/Users/AR/Desktop/Laura/02_Testmaterial/Drehung1.avi");
+//	//VideoCapture cap("C:/Users/AR/Desktop/Laura/02_Testmaterial/Drehung2.avi");
+//	//VideoCapture cap("C:/Users/AR/Desktop/Laura/02_Testmaterial/Lift1.avi");
+//	//VideoCapture cap("C:/Users/AR/Desktop/Laura/02_Testmaterial/Lift2.avi");
+//	//VideoCapture cap("C:/Users/AR/Desktop/Laura/02_Testmaterial/Register1.avi");
+//	//VideoCapture cap("C:/Users/AR/Desktop/Laura/02_Testmaterial/reinraus1.avi");
+//	//VideoCapture cap("C:/Users/AR/Desktop/Laura/02_Testmaterial/reinraus2.avi");
+//	//VideoCapture cap("C:/Users/AR/Desktop/Laura/02_Testmaterial/Swap1.avi");
+//	//VideoCapture cap("C:/Users/AR/Desktop/Laura/02_Testmaterial/Swap2.avi");
+//	//VideoCapture cap("C:/Users/AR/Desktop/Laura/02_Testmaterial/totalCover1.avi");
+//	//VideoCapture cap("C:/Users/AR/Desktop/Laura/02_Testmaterial/totalCover2.avi");
+//
+//	//alt
+//	//VideoCapture cap("C:/Users/AR/Desktop/Laura/01_Testmaterial/001_A_Ohne_Verdeckung.avi");
+//	//VideoCapture cap("C:/Users/AR/Desktop/Laura/01_Testmaterial/001_B_Ohne_Verdeckung.avi");
+//	//VideoCapture cap("C:/Users/AR/Desktop/Laura/01_Testmaterial/006_Nacheinander_Hineinschieben.avi");
+//	//VideoCapture cap("C:/Users/AR/Desktop/Laura/01_Testmaterial/002_A_Nichtmarkierte_Ecken_verdeckt.avi");
+//
+//	if (!cap.isOpened())  // check if we succeeded
+//		return -1;
+//#endif // VIDEOLAURA
+//
+//#ifdef VIDEOVERA
+//	//Einbindung Video Vera 
+//	VideoCapture cap("F:/Master/Masterprojekt/Testvideos/001_A_Ohne_Verdeckung.avi");
+//	//VideoCapture cap("F:/Master/Masterprojekt/Testvideos/001_B_Ohne_Verdeckung.avi");
+//	//VideoCapture cap("F:/Master/Masterprojekt/Testvideos/002_A_Nichtmarkierte_Ecken_verdeckt.avi");
+//	//VideoCapture cap("F:/Master/Masterprojekt/Testvideos/002_B_Nichtmarkierte_Ecken_verdeckt.avi");
+//	//VideoCapture cap("F:/Master/Masterprojekt/Testvideos/003_A_Markierte_Ecke_verdeckt.avi");
+//	//VideoCapture cap("F:/Master/Masterprojekt/Testvideos/003_B_Markierte_Ecke_verdeckt.avi");
+//	//VideoCapture cap("F:/Master/Masterprojekt/Testvideos/004_A_Person_verdeckt_Marker.avi");
+//	//VideoCapture cap("F:/Master/Masterprojekt/Testvideos/004_B_Person_verdeckt_Marker.avi");
+//	//VideoCapture cap("F:/Master/Masterprojekt/Testvideos/005_A_Farbige_Aermel.avi");
+//	//VideoCapture cap("F:/Master/Masterprojekt/Testvideos/005_B_Farbige_Aermel.avi");
+//	//VideoCapture cap("F:/Master/Masterprojekt/Testvideos/006_Nacheinander_Hineinschieben.avi");
+//	//VideoCapture cap("F:/Master/Masterprojekt/Testvideos/reinraus1.avi");
+//
+//	if (!cap.isOpened())  // check if we succeeded
+//		return -1;
+//#endif // VIDEOVERA
+//
+//
+//
+//#ifdef uEYE
+//	// uEye Caputure
+//	uEye_input* uei = new uEye_input();
+//	uei->inituEyeCam();
+//#endif // uEYE
+//
+//
+//	int counter = -1;
+//	Mat frame;
+//#ifdef uEYE
+//	for (int i = 0; i < 30; i++)
+//	{
+//		frame = uei->getCapturedFrame();
+//	}
+//#endif //uEYE
+//
+//	namedWindow("edges", 1);
+//	//LUT aufstellen
+//
+//	while (true)
+//	{
+//		counter++;
+//		printf("COUNTER %i \n ", counter);
+//#ifdef uEYE
+//		frame = uei->getCapturedFrame();
+//#endif // uEYE
+//
+//		//// Mark Middel for Calibration
+//		//line(frame, Point2f(630, 512), Point2f(650, 512), Scalar(255, 0, 0), 2, CV_AA);
+//		//line(frame, Point2f(640, 502), Point2f(640, 522), Scalar(255, 0, 0), 2, CV_AA);
+//		//cv::imshow("Captured Video", frame);
+//		//if (cv::waitKey(1) >= 0) break;
+//
+//#ifdef VIDEOLAURA
+//		cap >> frame; // get a new frame from camera
+//#endif // VIDEOLAURA
+//
+//
+//#ifdef VIDEOVERA
+//		cap >> frame; // get a new frame from camera
+//#endif // VIDEOVERA
+//
+//		if (!frame.empty()) {
+//			cvtColor(frame, frame, COLOR_BGR2HSV);
+//			// run Marker Detection
+//			MarkerDetection* md = new MarkerDetection();
+//			int sucess = md->runMarkerDetection(frame);
+//			if (sucess == 1) {
+//				std::vector<RotatedRect> rects = md->getDetectedRects();
+//				std::vector<unsigned char> markedCorners = md->getMarkedCorners();
+//
+//				// LUT Kalibration des Rechtecks
+//				//run MarkerManagement
+//
+//				for (int i = 0; i < rects.size(); i++)
+//				{
+//						mm->trackMarker(rects[i], markedCorners[i], frame.size());
+//				}
+//				marker = mm->getTrackedMarker();
+//				takenIdVec = mm->getTakenIDVec();
+//
+//				delete md;
+//
+//			}
+//			else { marker = mm->getTrackedMarker(); }
+//			debug(frame, marker, counter,takenIdVec);
+//
+//			imshow("edges", frame);
+//			if (waitKey(4) >= 0)break;
+//		}
+//		else break;
+//
+//#ifdef TCP
+//		//Send Markerdata via TCP
+//		out->sendTCPData(marker,takenIdVec);
+//
+//#endif // TCP
+//	}
+//#ifdef uEYE
+//	uei->exitCamera();
+//	delete uei;
+//#endif // uEYE
+//
+//	delete mm;
+//
+//
+//#ifdef TCP
+//	delete out;
+//#endif // TCP
+//	return EXIT_SUCCESS;
+//}
 
 
 void debug(Mat & frame, std::array<Marker*, 200> marker, int counter, std::vector<int> takenIDVec)
